574181b638414872cf986a58041d41ca4fa8845d
Carter Walsh <62627257+cartwrld@users.noreply.github.com>
2023-12-27 12:18:43 -0600

    added a colorbuddy for a custom color scheme

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -9,6 +9,10 @@ import {FaCode, FaImages} from 'react-icons/fa6'
 import {useRouter} from 'next/navigation';
 import styles from '../app/page.module.css'
 import {IoIosArrowForward, IoIosArrowBack} from "react-icons/io";
+import {AiTwotoneExperiment} from "react-icons/ai";
+import {ColorBuddy} from "@/utils/ColorBuddy";
+
+const cb = new ColorBuddy()
 
 
 type DashboardState = {
@@ -40,16 +44,14 @@ export default function Dashboard(currentPage: string) {
     const Divider6 = () => (<><Divider2/><Divider2/><Divider2/></>)
 
 
-
-
     return (
         <Flex className={isShrunk ? styles.expand : styles.shrink}
-              flexDir={'column'} bg={'gray.400'} alignItems={'center'}
+              flexDir={'column'} bg={'gray.500'} alignItems={'center'}
               roundedLeft={'20px'}
               roundedBottomStart={'15px'}
               shadow={'1px 0 10px rgba(0,0,0,0.1)'}>
             <Flex justifyContent={'center'} alignItems={'center'} bg={'gray.600'} w={'100%'} roundedTopLeft={'16px'}>
-                <Image src={'/images/cwlogo_final_transparent_blk.png'} alt={'cw_logo'} width={171} height={171}/>
+                <Image src={'/images/cwlogo_final_transparent_blk.png'} alt={'cw_logo'} width={200} height={200}/>
             </Flex>
             <Divider/>
             <Button onClick={handleToggleAnimation} mb={0} rounded={0} bg={'gray.600'}
@@ -68,47 +70,48 @@ export default function Dashboard(currentPage: string) {
 
             <Divider6/>
 
-            <Button onClick={() => navigate('generate')} mb={0} rounded={0} bg={'gray.500'} ps={8}
-                    width={'100%'} height={'fit-content'} py={3} justifyContent={'start'}>
-                <Flex pt={2} pb={2}>
-                    <FaImages fontSize={'2.1em'} me={2}/>
-                </Flex>
-                <Center className={isShrunk ? styles.shrinkAnimation : styles.expandAnimation}
-                        ms={4} pb={1} w={'70%'}>
-                    <Heading fontSize={'1.6em'} w={'100%'}>Generate</Heading>
-                </Center>
-            </Button>
+            {/*<Button onClick={() => navigate('generate')} mb={0} rounded={0} bg={'gray.500'} ps={8}*/}
+            {/*        width={'100%'} height={'fit-content'} py={3} justifyContent={'start'}>*/}
+            {/*    <Flex pt={2} pb={2}>*/}
+            {/*        <FaImages fontSize={'2.1em'} me={2}/>*/}
+            {/*    </Flex>*/}
+            {/*    <Center className={isShrunk ? styles.shrinkAnimation : styles.expandAnimation}*/}
+            {/*            ms={4} pb={1} w={'70%'}>*/}
+            {/*        <Heading fontSize={'1.6em'} w={'100%'}>Generate</Heading>*/}
+            {/*    </Center>*/}
+            {/*</Button>*/}
+            {DashboardBTN('generate', 'Generate')}
             <Divider2/>
-
-            <Button onClick={() => navigate('history')} mb={0} rounded={0} bg={'gray.500'} ps={8}
-                    width={'100%'} height={'fit-content'} py={3} justifyContent={'start'}>
-                <Flex pt={2} pb={2}>
-                    <FaHistory fontSize={'2.1em'} me={2}/>
-                </Flex>
-                <Center className={isShrunk ? styles.shrinkAnimation : styles.expandAnimation}
-                        ms={4} pb={1} w={'70%'}>
-                    <Heading fontSize={'1.6em'} w={'100%'}>History</Heading>
-                </Center>
-
-            </Button>
+            {DashboardBTN('history', 'History')}
             <Divider2/>
-            <Button onClick={() => navigate('testing')} mb={0} rounded={0} bg={'gray.500'} ps={8}
-                    width={'100%'} height={'fit-content'} py={3} justifyContent={'start'}>
-                <Flex pt={2} pb={2}>
-                    < FaCode fontSize={'2.1em'} me={2}/>
-                </Flex>
-                <Center className={isShrunk ? styles.shrinkAnimation : styles.expandAnimation}
-                        ms={4} pb={1} w={'70%'}>
-                    <Heading fontSize={'1.6em'} w={'100%'}>Testing</Heading>
-                </Center>
-            </Button>
+            {DashboardBTN('testing', 'Testing')}
             <Divider2/>
         </Flex>
     )
 
 
-
-
+    function DashboardBTN(route: string, title: string) {
+        return (
+            <Button onClick={() => navigate(route)} mb={0} rounded={0} bg={'#586476'} ps={8}
+                    width={'100%'} height={'fit-content'} py={3} justifyContent={'start'}>
+                <Flex pt={2} pb={2}>
+                    {route === 'generate'
+                        ? (
+                            <FaImages fontSize={'2.1em'} me={2}/>
+                        ) : route === 'history'
+                            ? (<FaHistory fontSize={'2.1em'} me={2}/>)
+                            : route === 'testing'
+                                ? (<AiTwotoneExperiment fontSize={'2.1em'} me={2}/>)
+                                : (<Center>error</Center>)
+                    }
+                </Flex>
+                <Center className={isShrunk ? styles.shrinkAnimation : styles.expandAnimation}
+                        ms={4} pb={1} w={'70%'}>
+                    <Heading fontSize={'1.6em'} w={'100%'}>{title}</Heading>
+                </Center>
+            </Button>
+        )
+    }
 
 
 }
--- src/utils/ComfyUtils.ts
+++ src/utils/ComfyUtils.ts
@@ -4,6 +4,8 @@ import Workflow from "@/utils/Workflow";
 import * as fs from 'fs'
 import path from 'path'
 
+
+
 const API_URL = `http://localhost:3004`
 
 export default async function execGeneration(workflowData: Workflow): Promise<string> {
@@ -23,19 +25,30 @@ export async function getHistory(orientation?: string): Promise<string> {
         let fetchURL = `${API_URL}/history`;
 
         switch (orientation) {
-            case 'square': fetchURL += '/square'; break
-            case 'portrait': fetchURL += '/portrait'; break
-            case 'landscape': fetchURL += '/landscape'; break
-            default: break;
+            case 'square':
+                fetchURL += '/square';
+                break
+            case 'portrait':
+                fetchURL += '/portrait';
+                break
+            case 'landscape':
+                fetchURL += '/landscape';
+                break
+            default:
+                break;
         }
         const response = await axios.get(fetchURL, {
             withCredentials: true
         });
         switch (orientation) {
-            case 'square': return response.data.squarePaths
-            case 'portrait': return response.data.portraitPaths
-            case 'landscape': return response.data.landscapePaths
-            default: return response.data.paths
+            case 'square':
+                return response.data.squarePaths
+            case 'portrait':
+                return response.data.portraitPaths
+            case 'landscape':
+                return response.data.landscapePaths
+            default:
+                return response.data.paths
         }
 
     } catch (error) {
@@ -59,16 +72,23 @@ export async function getDBHistory(): Promise<any[]> {
 }
 
 
-export async function getHistoryByOrientation(orientation?:string ): Promise<any[]> {
+export async function getHistoryByOrientation(orientation?: string): Promise<any[]> {
     try {
 
         let fetchURL = `${API_URL}/history/database`
 
         switch (orientation) {
-            case 'square': fetchURL += '/square'; break
-            case 'portrait': fetchURL += '/portrait'; break
-            case 'landscape': fetchURL += '/landscape'; break
-            default: break;
+            case 'square':
+                fetchURL += '/square';
+                break
+            case 'portrait':
+                fetchURL += '/portrait';
+                break
+            case 'landscape':
+                fetchURL += '/landscape';
+                break
+            default:
+                break;
         }
         const response = await axios.get(fetchURL, {
             withCredentials: true
