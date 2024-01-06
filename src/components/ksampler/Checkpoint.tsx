6f10b930adb7739e74a9a7321dfe6eb97ce16c91
Carter Walsh <62627257+cartwrld@users.noreply.github.com>
2024-01-06 13:02:21 -0600

    push before windows upgrade

--- src/components/ksampler/Checkpoint.tsx
+++ src/components/ksampler/Checkpoint.tsx
@@ -2,7 +2,7 @@ import {Component} from "react";
 import { Center, Flex, Heading, Select } from "@chakra-ui/react";
 
 
-const ckptlist = [
+const ckptlistXL = [
     'altxl_v50.safetensors',
     'ambienceSDXL_a1.safetensors',
     'animatediff_blossom.json',
@@ -53,6 +53,81 @@ const ckptlist = [
     'unstableDiffusers_HW_v8.safetensors',
     'zavychromaxl_v10.safetensors'
 ]
+
+let str = `
+
+3DAnimationDiffusion_v10.safetensors
+3DAnimationDiffusion_v10.sha256
+3DMixCharacter_Realism_v20.safetensors
+A7B3_v10.safetensors
+AbsoluteReality_v181.safetensors
+Anime3DMix_1.safetensors
+Animerge_v22.safetensors
+Aniverse_thx_v14.safetensors
+Anything_v3.ckpt
+Architectural_v31.safetensors
+ArthemyComics_v50.safetensors
+ArtUniverse_v80.safetensors
+AZovyaRPGArtistTools_v3.safetensors
+Babes_v20.safetensors
+BeautifulRealistic_v7.safetensors
+Beautyfool_v20.safetensors
+BetterDisneyPixar_v10.safetensors
+BrainDance_v071.safetensors
+chilloutmix_v10.safetensors
+CleanLinearMix_final.safetensors
+Colorful_v31.safetensors
+DarkSushi_v40.safetensors
+DreamShaper_v8.safetensors
+DungeonsAndDiffusion_v3.safetensors
+EpicPanty_v1.safetensors
+EpicPhotogasm_xPlusPlus_v1.safetensors
+EpicPhotogasm_y_v1.safetensors
+EpicRealism_natural_v1.safetensors
+EpicRealism_pe_v5.safetensors
+Faetastic_v2.safetensors
+FantasticMix_k1_v1.safetensors
+FrottagePatterns_v1.ckpt
+GhostMix_v20.safetensors
+GoliathPerson_DND_v1.ckpt
+HelloYoung25d_v10.safetensors
+IncredibleWorld_v40.safetensors
+KeybitSpace_v07.safetensors
+MajicMix_Realistic_v7.safetensors
+MeinaMix_v110.safetensors
+MeinaMix_v8.safetensors
+MeinaMix_Pastel_v6.safetensors
+YorozaStyle_NightSky_v1.safetensors
+OmegaCentauri_AlphaMain_v1.safetensors
+PerfectDeliberate_v10.safetensors
+PerfectDeliberate_v40.safetensors
+Pope25_v14.safetensors
+RC_3d_v8.safetensors
+RC_Anime_v5.safetensors
+RC_Pixar_v2.safetensors
+StableYogi_Realism_v10.safetensors
+RealisticVision_v51.safetensors
+RevAnimated_v122.safetensors
+Samaritan3dCartoon_v3.safetensors
+SVDN3_RealArt_v1.safetensors
+sd_1.5.safetensors
+Silicon29_v1.safetensors
+SilverMoonMix_v13.safetensors
+Soapmix28D_v10.safetensors
+ThisIsReal_v50.safetensors
+TinyPlanets_v1.ckpt
+UniverseStable_AnalogPhoto_v70.safetensors
+VirileAnimation_v10.safetensors
+VirileFantasy_v11.safetensors
+ZavyComics_b1_v1.safetensors
+ZavyMix_v10.safetensors
+Zero_v1.safetensors
+
+
+`
+
+
+
 
 type CKPTState = {
     ckptValue: string;
@@ -105,7 +180,7 @@ export class Checkpoint extends Component<CKPTProps, CKPTState> {
                             paddingTop: '10px', backgroundColor: `gray.700`, borderRadius: '8px'
                         }}}>
                 {
-                    ckptlist.map((ckpt, index) => (<option key={index} value={ckpt}
+                    ckptlistXL.map((ckpt, index) => (<option key={index} value={ckpt}
                                                         style={{
                                                             backgroundColor: '#f4f9ff', // Example style
                                                         }}>{ckpt}</option>))}
