00007e10db147d7952b83c777bbdeccc01fca3ab
Carter Walsh <cartwalsh@gmail.com>
2024-01-10 20:23:16 -0600

    cosa playground for design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 'use client'
 
 import styles from './page.module.css'
-import {Center, Flex} from "@chakra-ui/react";
+import {Box, Button, Center, Container, Divider, Flex, Heading, RadioGroup, Stack} from "@chakra-ui/react";
 import {KSampler} from "@/components/ksampler/KSampler";
 import {GeneratedImage} from "@/components/ksampler/GeneratedImage";
 import {BaseShell} from "@/components/ksampler/BaseShell";
 import React, {useState} from "react";
 import {auto} from "@popperjs/core";
 import NavDrawer from "@/components/NavDrawer";
-
-
+import {FaAnglesLeft, FaAnglesRight} from "react-icons/fa6";
+import {PartMachineRelations} from "@/components/proj/PartMachineRelations";
+import MachineSelector from "@/components/proj/MachineSelector";
 
 export default function Home() {
 
@@ -19,13 +20,19 @@ export default function Home() {
         <>
             <NavDrawer></NavDrawer>
         <main className={styles.main}>
-
-
-            <Flex flexDir={'row'} justifyContent={'space-around'} w={'98%'} alignItems={'center'}>
-
+            <Flex flexDir={'row'} justifyContent={'center'} w={'98%'} alignItems={'center'}>
+                <MachineSelector />
             </Flex>
         </main>
         </>
     );
 }
 
+
+
+function retrieveExistingMachinesFromDB() {
+
+}
+function retrieveAllMachinesFromDB() {
+
+}
